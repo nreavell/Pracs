c953dcb19d39463ba9fc9735d4faef061563f0fa
nreavell <nathan.reavell@my.jcu.edu.au>
2017-08-30 14:45:25 +1000

    Add word_dictionary.py

---
+++ prac_05/word_dictionary.py
@@ -0,0 +1,19 @@
+
+text = input("Text: ")
+word_total = text.split()
+
+known_words = {}
+
+for word in word_total:
+    word_amount = known_words.get(word, 0)
+    if word_amount is None:
+        known_words[word] = 1
+    else:
+        known_words[word] = word_amount + 1
+
+word_total = list(known_words.keys())
+word_total.sort()
+max_length = max((len(word) for word in word_total))
+
+for word in word_total:
+    print("{:{}} : {}".format(word, max_length, known_words[word]))
