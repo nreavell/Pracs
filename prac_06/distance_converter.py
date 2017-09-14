10c1852ae4ef7a5b21d857fd757a5e64880a9b8a
nreavell <nathan.reavell@my.jcu.edu.au>
2017-09-14 11:46:03 +1000

    Add name_loop

--- prac_06/distance_converter.py
+++ prac_06/distance_converter.py
@@ -11,7 +11,7 @@ class MilesConverterApp(App):
         self.root = Builder.load_file('distance_converter.kv')
         return self.root
 
-    def handle_calculate(self):
+    def handle_convert(self):
         value = self.get_valid_miles()
         result = value * miles_to_km
         self.root.ids.output_label.text = str(result)
@@ -19,7 +19,7 @@ class MilesConverterApp(App):
     def handle_increment(self, change):
         value = self.get_valid_miles() + change
         self.root.ids.input_miles.text = str(value)
-        self.handle_calculate()
+        self.handle_convert()
 
     def get_valid_miles(self):
         try:
